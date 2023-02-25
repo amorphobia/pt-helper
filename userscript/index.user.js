7726fd7932b52672dcf43b7fcef4a56319b54d2a
Xuesong Peng <pengxuesong.cn@gmail.com>
2023-02-25 16:19:55 +0800

    fix(pterclub): direct link copy button not added

--- userscript/index.user.js
+++ userscript/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name PT Helper
 // @name:zh-CN PT 助手
-// @version 0.1.6
+// @version 0.1.7
 // @namespace https://github.com/amorphobia/pt-helper
 // @description A helper for private trackers
 // @description:zh-CN 私密种子站点的助手
@@ -4821,6 +4821,47 @@ table.head {
     onLoad() {
         super.onLoad();
         this.attendance();
+    }
+    addDirectLink() {
+        if (!this.getHostValue("directLink") || this.passkey == "") {
+            return;
+        }
+        const id_re = /id=\d+/;
+        const trs = document.querySelectorAll("table.torrentname > tbody > tr:nth-of-type(1)");
+        for (const tr of trs) {
+            const tds = tr.querySelectorAll("td");
+            if (!tds || tds.length < 5) {
+                continue;
+            }
+            const dl = tds[3].querySelector("a");
+            const direct_link = dl ? dl.href : "";
+            if (direct_link == "") {
+                continue;
+            }
+            const img = document.createElement("img");
+            img.setAttribute("src", "pic/trans.gif");
+            img.setAttribute("class", "torrent_direct_link");
+            img.setAttribute("alt", "DL");
+            const a = document.createElement("a");
+            a.setAttribute("title", "左键单击复制，链接中包含个人秘钥Passkey，切勿泄露！");
+            a.setAttribute("onclick", "return false");
+            a.setAttribute("id", "direct_link");
+            a.setAttribute("href", direct_link);
+            a.setAttribute("data-clipboard-text", direct_link);
+            a.appendChild(img);
+            tds[4].prepend(a);
+        }
+        this.css += `
+.swal2-container {
+    z-index: 4294967295;
+}
+img.torrent_direct_link {
+    width: 16px;
+    height: 16px;
+    background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAH5QTFRFR3BMyKN4cz0Td3d3sLCw6enp////qHg4oaGhcz0TlpaWkV8opnU2lmQrjVklqHg4m2kvo3I03ruJiFMhn24y4r+N2raG5cSP9+jQg04e1rKD68uUnYpv6ceS0q5/fkkaoaGhzqp8h4eHr6+v+Pj4ekQXdkAV+/v78fHxy6Z6f0p3WgAAAAp0Uk5TAP///////5aWlrne7esAAACHSURBVBjTbc5HEsIwEERRA5qxLeecc77/BTEN0oq/m1ddKhnG36xxtPRhBq2UxyFlG5gAt5zXk+hc59IFRM3yQksTAdKOf3UpICxYCEFEXIQAL2NCnHkAJ/4s7jh2AH6uFrkPSOrvgrhOAFWvFn0FGCYWhDemAbBd6h/XBtgfuh1gP3X2fb4BlrkIUt3i2kgAAAAASUVORK5CYII=');
+    padding-bottom: 1px;
+}`;
+        this.registerClipboard("#direct_link");
     }
     attendance() {
         if (!this.getHostValue("attendance")) {
