396633efaaaee78bdf346edfc881ed1bb4d5c17c
Xuesong Peng <pengxuesong.cn@gmail.com>
2023-03-01 11:09:52 +0800

    chore: bump version 0.1.10

--- userscript/index.user.js
+++ userscript/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name PT Helper
 // @name:zh-CN PT 助手
-// @version 0.1.9
+// @version 0.1.10
 // @namespace https://github.com/amorphobia/pt-helper
 // @description A helper for private trackers
 // @description:zh-CN 私密种子站点的助手
@@ -15,6 +15,7 @@
 // @match *://pt.sjtu.edu.cn/*
 // @match *://pterclub.com/*
 // @match *://tjupt.org/*
+// @match *://www.hdarea.co/*
 // @require https://cdn.jsdelivr.net/npm/sweetalert2@11.4.8
 // @require https://cdn.jsdelivr.net/npm/clipboard@2.0.11
 // @grant GM_addStyle
@@ -4813,10 +4814,9 @@ table.mainouter {
         this.attendance();
     }
     addDirectLink() {
-        if (!this.getHostValue("directLink") || this.passkey == "") {
+        if (!this.getHostValue("directLink")) {
             return;
         }
-        const id_re = /id=\d+/;
         const trs = document.querySelectorAll("table.torrentname > tbody > tr:nth-of-type(1)");
         for (const tr of trs) {
             const tds = tr.querySelectorAll("td");
@@ -4989,6 +4989,112 @@ class TJUPT extends index_1.NexusPHP {
 exports.TJUPT = TJUPT;
 
 
+/***/ }),
+/* 10 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.HDarea = void 0;
+const NexusPHP_1 = __webpack_require__(2);
+const common_1 = __webpack_require__(3);
+class HDarea extends NexusPHP_1.NexusPHP {
+    constructor() {
+        super("www.hdarea.co");
+        this.menu_items = [
+            {
+                "id": "bannerHide",
+                "type": "switch",
+                "display": "隐藏横幅",
+                "name": "隐藏横幅",
+                "value": false
+            },
+            {
+                "id": "attendance",
+                "type": "switch",
+                "display": "自动签到",
+                "name": "自动签到",
+                "value": true
+            }
+        ].concat(this.menu_items);
+    }
+    onLoad() {
+        super.onLoad();
+        this.attendance();
+    }
+    tweakBanner() {
+        if (this.getHostValue("bannerHide")) {
+            this.css += `
+table.head {
+    display: none;
+}
+table.mainouter {
+    margin-top: 20px;
+}`;
+        }
+    }
+    addDirectLink() {
+        var _a;
+        if (!this.getHostValue("directLink") || this.passkey == "") {
+            return;
+        }
+        const id_re = /id=[\d]+/;
+        const trs = document.querySelectorAll("table.torrentname > tbody > tr:nth-of-type(1");
+        for (const tr of trs) {
+            const tds = tr.querySelectorAll("td");
+            if (!tds || tds.length < 4) {
+                continue;
+            }
+            const dl = tds[3].querySelector("a");
+            const result = id_re.exec((_a = dl === null || dl === void 0 ? void 0 : dl.href) !== null && _a !== void 0 ? _a : "");
+            if (!result) {
+                continue;
+            }
+            const direct_link = `https://${this.host}/download.php?${result[0]}&passkey=${this.passkey}`;
+            const img = document.createElement("img");
+            img.setAttribute("src", "pic/trans.gif");
+            img.setAttribute("class", "torrent_direct_link");
+            img.setAttribute("alt", "DL");
+            const a = document.createElement("a");
+            a.setAttribute("title", "左键单击复制，链接中包含个人秘钥Passkey，切勿泄露！");
+            a.setAttribute("onclick", "return false");
+            a.setAttribute("id", "direct_link");
+            a.setAttribute("href", direct_link);
+            a.setAttribute("data-clipboard-text", direct_link);
+            a.appendChild(img);
+            tds[3].prepend(a);
+        }
+        this.css += `
+.swal2-container {
+    z-index: 4294967295;
+}
+h2#swal2-title {
+    background-color: transparent;
+}
+img.torrent_direct_link {
+    width: 16px;
+    height: 16px;
+    background: url('${common_1.direct_link_img_url}');
+    paddint-bottom: 1px;
+}`;
+        this.registerClipboard("#direct_link");
+    }
+    attendance() {
+        if (!this.getHostValue("attendance")) {
+            return;
+        }
+        const attend = document.querySelector("span#sign_in > a");
+        if (attend) {
+            this.wait(2000).then(() => {
+                attend.click();
+            });
+        }
+    }
+}
+exports.HDarea = HDarea;
+
+
 /***/ })
 /******/ 	]);
 /************************************************************************/
@@ -5029,13 +5135,15 @@ const nanyangpt_com_1 = __webpack_require__(6);
 const pt_sjtu_edu_cn_1 = __webpack_require__(7);
 const pterclub_com_1 = __webpack_require__(8);
 const index_1 = __webpack_require__(9);
+const www_hdarea_co_1 = __webpack_require__(10);
 const host = window.location.host;
 const sites = new Map([
     ["hhanclub.top", new hhanclub_top_1.Hhanclub()],
     ["nanyangpt.com", new nanyangpt_com_1.NanyangPT()],
     ["pt.sjtu.edu.cn", new pt_sjtu_edu_cn_1.SJTU()],
     ["pterclub.com", new pterclub_com_1.Pterclub()],
-    ["tjupt.org", new index_1.TJUPT()]
+    ["tjupt.org", new index_1.TJUPT()],
+    ["www.hdarea.co", new www_hdarea_co_1.HDarea()],
 ]);
 const site = sites.get(host);
 if (site) {
